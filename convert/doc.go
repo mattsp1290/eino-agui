d279f1b850c3b3cf1a618a5ae43ab002eb774785
Matt Spurlin <mattsp1290@gmail.com>
2026-06-26 17:00:22 -0400

    build: initialize eino-agui module

---
+++ convert/doc.go
@@ -0,0 +1,3 @@
+// Package convert will contain bidirectional conversion between AG-UI messages
+// and eino schema messages.
+package convert
---
+++ stream/doc.go
@@ -0,0 +1,2 @@
+// Package stream will contain the reusable eino stream to AG-UI event tap.
+package stream
